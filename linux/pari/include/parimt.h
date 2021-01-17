293bd727c938e4ac2ef8c122590ed517bed39c7a
Lukas Valine <lukas@valine.io>
2021-01-17 17:30:50 -0600

    Update pari

--- linux/pari/include/parimt.h
+++ linux/pari/include/parimt.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2000  The PARI group.
+/* Copyright (C) 2013  The PARI group.
 
 This file is part of the PARI/GP package.
 
@@ -12,31 +12,5 @@ Check the License for details. You should have received a copy of it, along
 with the package; see the file 'COPYING'. If not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 
-enum err_list {
-/* Force errors into non-0 */
-  e_SYNTAX = 1, e_BUG,
-
-  e_ALARM, e_FILE,
-
-  e_MISC, e_FLAG, e_IMPL, e_ARCH, e_PACKAGE, e_NOTFUNC,
-
-  e_PREC, e_TYPE, e_DIM, e_VAR, e_PRIORITY, e_USER,
-
-  e_STACK, e_STACKTHREAD, e_OVERFLOW, e_DOMAIN, e_COMPONENT,
-
-  e_MAXPRIME,
-
-  e_CONSTPOL, e_IRREDPOL, e_COPRIME, e_PRIME, e_MODULUS, e_ROOTS0,
-
-  e_OP, e_TYPE2, e_INV,
-
-  e_MEM,
-
-  e_SQRTN,
-
-  e_FILEDESC,
-/* NO ERROR */
-  e_NONE
-};
-
-enum { warner, warnprec, warnfile, warnmem, warnuser, warnstack, warnstackthread };
+#define MT_SIGINT_BLOCK(block)
+#define MT_SIGINT_UNBLOCK(block)
